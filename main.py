52565f2a38ab2da8319c0c65cdd393e6db2fe9be
PCU01 <motolik2018@gmail.com>
2023-10-24 18:24:49 +0200

    pridal jsem ctverec

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def obdelnicek():
         left(90)
         forward(200)
         left(90)
+def ctverec():
+    for i in range(4):
+        ctverecek()
 
 def obdelnik():
     for i in range(2):
         obdelnicek()
 tvar = ""
-while tvar != "obdelnik":
+while tvar != "ctverec" and tvar != "obdelnik":
     tvar = textinput("ahoj", "co mam kreslit: ")
 
 if tvar == "ctverec":
